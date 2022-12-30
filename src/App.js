ba8251c303a1b8f1aa4d58e8893972b9acb2e974
Gopal Ganji <siddhu.bvs@gmail.com>
2022-12-30 19:55:57 +0530

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './twitter.svg'
+import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import {FaCrosshairs} from 'react-icons/fa'
 
 function App() {
+  const [trends, setTrends] = useState([])
+  const [woeid, setWoeid] = useState('1')
+
+  useEffect(() => getTrends(), [woeid])
+
+  function getTrends() {
+    axios
+      .get('/api/trends', {
+        params: {
+          woeid,
+        },
+      })
+      .then(response => {
+        setTrends(response.data[0].trends)
+      })
+      .catch(error => console.log(error.message))
+  }
+  
+  function handleLocation(){
+    alert('handle location')
+  }
+  function listTrends() {
+    return (
+      <ul>
+        {trends.map((trend, index) => {
+          return (
+            <li key={index}>
+              <a href={trend.url}>{trend.name}</a>
+              </li>
+          )
+        })}
+      </ul>
+    )
+  }  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='logo' alt='twitter' />
+        <h3>Twitter Trends</h3>
       </header>
-    </div>
-  );
+      <div className='menu'>
+        <select name='trending-place' onChange={e=>setWoeid(e.target.value)}>
+          <option value='1'>Worldwide</option>
+          <option value='23424848'>India</option>
+          <option value='2459115'>New York,US</option>
+          <option value='44418'>London,UK</option>
+          <option value='615702'>Paris</option>
+          <option value='1105779'>Sydney,AU</option>
+        </select>
+        <div className="location" onClick={handleLocation}>
+        <FaCrosshairs/>
+        </div>
+        </div>
+        <div className='content'>{listTrends()}</div>
+        </div>
+  )
 }
 
-export default App;
+export default App
+ 
